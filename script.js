2a712e3d18fa555931d1a8d024242bc0adb91906
Fernando <fernandoborgesjr@outlook.com>
2022-08-17 11:18:31 -0300

    feat: adiciona accordion no faq

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+initTabNav();
+initAccordion();
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("active");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-  tabMenu.forEach((item, index) => {
-    item.addEventListener("click", () => activeTab(index));
-  });
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("active");
 
-  function activeTab(index) {
-    if (tabContent[index].classList.contains("active")) {
-      return;
-    }
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => activeTab(index));
     });
-    tabContent[index].classList.add("active");
+
+    function activeTab(index) {
+      if (tabContent[index].classList.contains("active")) {
+        return;
+      }
+      tabContent.forEach((section) => {
+        section.classList.remove("active");
+      });
+      tabContent[index].classList.add("active");
+    }
+  }
+}
+
+function initAccordion() {
+  const accordionListTitles = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionListTitles.length) {
+    accordionListTitles.forEach((item) =>
+      item.addEventListener("click", activeAccordion)
+    );
+
+    accordionListTitles[0].click();
+  }
+
+  function activeAccordion() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("active");
   }
 }
