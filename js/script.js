068f00519aabfefd0c2ed5126609add7a90b39e2
Fernando <fernandoborgesjr@outlook.com>
2022-10-29 12:38:14 -0300

    feat(tooltip): adiciona o tooltip

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 import { initModal } from "./modules/modal.js";
 import { initTabNav } from "./modules/tabnav.js";
+import { initTooltip } from "./modules/tooltip.js";
 import { initAccordion } from "./modules/accordion.js";
 import { initAnimateScroll } from "./modules/scroll-animation.js";
 
 initModal();
 initTabNav();
+initTooltip();
 initAccordion();
 initAnimateScroll();
