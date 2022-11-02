601cf84246cfa440fc9621a3d54068b6f95b8afc
Fernando <fernandoborgesjr@outlook.com>
2022-11-02 13:26:53 -0300

    feat: adiciona o menu dropdown

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ import { initModal } from "./modules/modal.js";
 import { initTabNav } from "./modules/tabnav.js";
 import { initTooltip } from "./modules/tooltip.js";
 import { initAccordion } from "./modules/accordion.js";
+import { initDropdownMenu } from "./modules/dropdown-menu.js";
 import { initAnimateScroll } from "./modules/scroll-animation.js";
 
 initModal();
 initTabNav();
 initTooltip();
 initAccordion();
+initDropdownMenu();
 initAnimateScroll();
