f9a4bc7cb7a5f7865e0663ad3efc840f8f632aef
Fernando <fernandoborgesjr@outlook.com>
2024-01-28 12:28:33 -0300

    refact(accordion): convert to class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,14 +1,23 @@
-export function initAccordion() {
-  const accordionListTitles = document.querySelectorAll("[data-anime='accordion'] dt");
-
-  if (accordionListTitles.length) {
-    accordionListTitles.forEach((item) => item.addEventListener('click', activeAccordion));
-
-    accordionListTitles[0].click();
+export class Accordion {
+  constructor(listSelector) {
+    this.list = document.querySelectorAll(listSelector);
+    this.activeClass = 'active';
   }
 
-  function activeAccordion() {
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
+  init() {
+    if (this.list.length) {
+      this.addEvent();
+
+      this.toggle(this.list[0]);
+    }
+  }
+
+  addEvent() {
+    this.list.forEach((item) => item.addEventListener('click', () => this.toggle(item)));
+  }
+
+  toggle(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import { initModal } from './modules/modal.js';
 import { initTabNav } from './modules/tabnav.js';
 import { initTooltip } from './modules/tooltip.js';
-import { initAccordion } from './modules/accordion.js';
+import { Accordion } from './modules/accordion.js';
 import { initOperation } from './modules/operation.js';
 import { initMenuMobile } from './modules/menu-mobile.js';
 import { initDropdownMenu } from './modules/dropdown-menu.js';
@@ -11,9 +11,11 @@ import { initAnimateNumbers } from './modules/numbers-animation.js';
 initModal();
 initTabNav();
 initTooltip();
-initAccordion();
 initOperation();
 initMenuMobile();
 initDropdownMenu();
 initAnimateScroll();
 initAnimateNumbers();
+
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
